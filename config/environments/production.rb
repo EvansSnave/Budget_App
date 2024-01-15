37954095c889168f86c50f91ae293cf31fdb66db
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-15 13:32:17 -0500

    update production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { host: 'dpg-cmi7hpta73kc739khu8g-a', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'dpg-cmi7hpta73kc739khu8g-a', port: 3000 }
 
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
