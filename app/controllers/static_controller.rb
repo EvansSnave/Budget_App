3b0f23da2ca8e5f269953f8cbccd922d3b793fa1
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-14 22:15:55 -0500

    delete testing files

--- app/controllers/static_controller.rb
+++
@@ -1,5 +0,0 @@
-class StaticController < ApplicationController
-  def home
-    @user = User.create(name: 'Kevin')
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_15_020756) do
+ActiveRecord::Schema[7.1].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
