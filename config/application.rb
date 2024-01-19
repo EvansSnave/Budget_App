c7764982c07e3d5eb26bad097488a96435a28ae2
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-19 16:23:23 -0500

    ddeed

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module BudgetApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.autoloader = :classic
   end
 end
