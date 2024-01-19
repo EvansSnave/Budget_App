457ef945bfc012cc8c95fb20128dd68332b37bb2
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-19 16:58:25 -0500

    fix linter errors

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,13 +10,15 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
     @available_icons = ['fas fa-car', 'fas fa-home', 'fas fa-bicycle', 'fas fa-bus', 'fas fa-shopping-cart',
-                        'fas fa-utensils', 'fas fa-plane', 'fas fa-money-bill-wave', 'fas fa-hamburger', 'fas fa-university']
+                        'fas fa-utensils', 'fas fa-plane', 'fas fa-money-bill-wave', 'fas fa-hamburger',
+                        'fas fa-university']
   end
 
   def create
     @group = current_user.groups.build(group_params)
     @available_icons = ['fas fa-car', 'fas fa-home', 'fas fa-bicycle', 'fas fa-bus', 'fas fa-shopping-cart',
-      'fas fa-utensils', 'fas fa-plane', 'fas fa-money-bill-wave', 'fas fa-hamburger', 'fas fa-university']
+                        'fas fa-utensils', 'fas fa-plane', 'fas fa-money-bill-wave', 'fas fa-hamburger',
+                        'fas fa-university']
 
     if @group.save
       redirect_to root_path, notice: 'Category created successfully!'
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Group, type: :model do
   let(:user) { create(:user) } # Creating a user instance
 
-  subject { build(:group, user: user) } # Ensuring the group instance has an associated user
+  subject { build(:group, user:) } # Ensuring the group instance has an associated user
 
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
